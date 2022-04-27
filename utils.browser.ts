7def76e3bd4b897b5221c9bc81e01448dbc70012
ShenHongFei <shen.hongfei@outlook.com>
2022-04-27 14:55:08 +0800

    utils.browser.ts add delta2str;

--- utils.browser.ts
+++ utils.browser.ts
@@ -12,7 +12,7 @@ export function concat (arrays: ArrayBufferView[]) {
     let length = 0
     for (const a of arrays)
         length += a.byteLength
-        
+    
     let buf = new Uint8Array(length)
     let offset = 0
     for (const a of arrays) {
@@ -25,9 +25,32 @@ export function concat (arrays: ArrayBufferView[]) {
 }
 
 
+/** 时间间隔 (milliseconds) 格式化 */
+export function delta2str (delta: number) {
+    // [0, 1000) ms
+    if (delta < 1000)
+        return `${delta} ms`
+    
+    // 1.123 s
+    if (1000 <= delta && delta < 1000 * 60)
+        return `${(delta / 1000).toFixed(1)} s`
+    
+    // 1 min 12 s [1 min 0s, 60 min)
+    const seconds = delta / 1000
+    
+    if (seconds < 60 * 60)
+        return `${Math.trunc(seconds / 60)} min ${seconds % 60} s`
+    
+    const hour = Math.trunc(seconds / 3600)
+    
+    return `${hour} h ${Math.trunc((seconds - 3600 * hour) / 60)} min ${seconds % 60} s`
+}
+
+
 /** 字符串字典序比较 */
 export function strcmp (l: string, r: string) {
     if (l === r) return 0
     if (l < r)   return -1
     return 1
 }
+
